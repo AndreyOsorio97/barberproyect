d530de922ed63e6a68dfad207807219ca13bdb12
Link1393 <edsonandree@gmail.com>
2023-05-13 06:29:00 -0500

    Test

--- src/main/java/com/example/Barber_Project/BarberProjectApplication.java
+++ src/main/java/com/example/Barber_Project/BarberProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BarberProjectApplication {
-
+//This is a test
 	public static void main(String[] args) {
 		SpringApplication.run(BarberProjectApplication.class, args);
 	}
